f1beb939e92b27b0a33cbe9870d314fb2bd1c61f
programmer <internetov.ru@gmail.com>
2021-01-14 20:20:57 +0300

    add tests annotations

--- tests/DependencyChecker/ServiceTest.php
+++ tests/DependencyChecker/ServiceTest.php
@@ -18,10 +18,18 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \DepCheck\DependencyChecker\Service
+ * @covers \DepCheck\DependencyChecker\Rules
+ * @covers \DepCheck\DependencyChecker\Element
+ * @covers \DepCheck\DependencyChecker\Layer
+ * @covers \DepCheck\DependencyChecker\Dependency
+ * @covers \DepCheck\DependencyChecker\Result\Report
+ *
  */
 class ServiceTest extends TestCase
 {
-
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\UnknownElement
+     */
     public function testUnknown(): void
     {
         $rules = new Rules();
@@ -35,6 +43,9 @@ class ServiceTest extends TestCase
         $this->assertEquals(new UnknownElement($elements[0]), $result->records[0]);
     }
 
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\Allowed
+     */
     public function testAllowed(): void
     {
         $rules = new Rules();
@@ -55,6 +66,9 @@ class ServiceTest extends TestCase
 
     }
 
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\Forbidden
+     */
     public function testForbidden(): void
     {
         $rules = new Rules();
@@ -74,6 +88,10 @@ class ServiceTest extends TestCase
         $this->assertEquals(new Forbidden($el1, $layerA, $el2, $layerB), $result->records[0]);
     }
 
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\UnknownElement
+     * @covers \DepCheck\DependencyChecker\Result\UnknownDependsOn
+     */
     public function testUnknownDependsOn(): void
     {
         $layerB = new Layer('b');
@@ -89,6 +107,10 @@ class ServiceTest extends TestCase
         $this->assertEquals(new UnknownDependsOn($el1, $el2, $layerB), $result->records[1]);
     }
 
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\UnknownElement
+     * @covers \DepCheck\DependencyChecker\Result\UnknownDependsOnUnknown
+     */
     public function testUnknownDependsOnUnknown(): void
     {
         $el2 = new Element('id2', [], []);
@@ -97,12 +119,16 @@ class ServiceTest extends TestCase
 
         $checker = new Service(new Rules());
         $result = $checker->check($elements);
-
-        $this->assertEquals(2, $result->total);
+        $this->assertEquals(3, $result->total);
         $this->assertEquals(new UnknownElement($el1), $result->records[0]);
         $this->assertEquals(new UnknownDependsOnUnknown($el1, $el2), $result->records[1]);
+        $this->assertEquals(new UnknownElement($el2), $result->records[2]);
     }
 
+    /**
+     * @covers \DepCheck\DependencyChecker\Result\UnknownElement
+     * @covers \DepCheck\DependencyChecker\Result\DependsOnUnknown
+     */
     public function testDependsOnUnknown(): void
     {
         $layerA = new Layer('a');
