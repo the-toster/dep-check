5b030c62f3d6900706d65ea2d861be78ce877149
programmer <internetov.ru@gmail.com>
2021-02-25 11:59:39 +0300

    WIP: test class prop handler

--- tests/NodesCollector/Handler/ClassPropTest.php
+++ tests/NodesCollector/Handler/ClassPropTest.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 namespace Tests\NodesCollector\Handler;
 
 
+use DepCheck\Model\Input\Node;
+use DepCheck\Model\Input\NodeDependency;
+use DepCheck\Model\Input\NodePosition;
+use DepCheck\Model\Input\Properties;
 use DepCheck\NodesCollector\Handlers\DeclarationHandlers\ClassProperty;
 use DepCheck\NodesCollector\Handlers\NodeCollection;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\PropertyProperty;
+use PHPUnit\Framework\TestCase;
 
-final class ClassPropTest
+final class ClassPropTest extends TestCase
 {
     /** @test */
     public function it_collects_prop_type(): void
@@ -22,5 +27,29 @@ final class ClassPropTest
                              ['parent'=>new Name('SomeClass')],
 
         );
+
+        $expected = [
+            'SomeClass' => $this->buildNode('SomeClass', []),
+            ''
+        ];
+
+        $handler->handle($prop);
+        $this->assertEquals($expected, $handler->getNodes());
     }
+
+    protected function buildDep(Node $node, int $line, int $col, int $type): NodeDependency
+    {
+        return new NodeDependency($node, new NodePosition($line, $col, ''), $type);
+    }
+
+    /**
+     * @param string $id
+     * @param NodeDependency[] $deps
+     * @return Node
+     */
+    private function buildNode($id, $deps = []): Node
+    {
+        return new Node($id, $deps, new Properties(''));
+    }
+
 }
